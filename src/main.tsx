4c28b2d4719dedb12b784bb2cfa61beb782b2136
Vladosha <vladaservetnik@gmail.com>
2025-03-15 15:55:17 +0200

    project setting

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
     <Toaster position="top-right"/>
